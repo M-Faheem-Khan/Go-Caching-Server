e9f33b695b080ce6b7241381abf8127c48d00b42
Muhammad Faheem Khan <faheem5948@gmail.com>
2022-02-11 00:04:34 -0500

    adding mongodb

--- src/main.go
+++ src/main.go
@@ -4,21 +4,43 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"github.com/go-redis/redis/v8"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
 	"log"
 	"net/http"
 	"os"
 	"strconv"
 	"strings"
-
-	"github.com/go-redis/redis/v8"
 )
 
-var REDIS_PASSWORD = os.Getenv("REDIS_PASSWORD")
-var client *redis.Client
+var redisClient *redis.Client
 var redis_ctx = context.Background()
+var usersCollection *mongo.Collection
+
+func getUserDataFromMongo(userId int) {
+	filter := bson.D{
+		{"$and",
+			bson.A{
+				bson.D{
+					{"id", bson.D{{"$eq", userId}}},
+				},
+			},
+		},
+	}
+	var result bson.M
+	err := usersCollection.FindOne(context.TODO(), filter).Decode(&result)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("displaying the first result from the search filter")
+	fmt.Println(result)
+}
 
 func getUserDataFromCache(userId int) (string, error) {
-	userData, err := client.Get(redis_ctx, strconv.Itoa(userId)).Result()
+	userData, err := redisClient.Get(redis_ctx, strconv.Itoa(userId)).Result()
 	if err != nil {
 		return "", err
 	}
@@ -54,7 +76,8 @@ func user(w http.ResponseWriter, r *http.Request) {
 		userData, err := getUserDataFromCache(userID)
 		if err != nil {
 			// Fetch user data from mongodb
-			// add to redis
+			getUserDataFromMongo(userID)
+			// add user data to redis
 			// send user data to requester
 			fmt.Fprintf(w, "Error: %s\n", err.Error())
 		} else {
@@ -69,15 +92,21 @@ func hello(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	var mongoDbName = "Twitch"
+	var mongoCollectionName = "Users"
+	var mongoUsername = os.Getenv("MONGO_USERNAME")
+	var mongoPassword = os.Getenv("MONGO_PASSWORD")
+	var redisPassword = os.Getenv("REDIS_PASSWORD")
+
 	// Creating Redis Client
-	client = redis.NewClient(&redis.Options{
+	redisClient = redis.NewClient(&redis.Options{
 		Addr:     "redis-server:6379",
-		Password: REDIS_PASSWORD,
+		Password: redisPassword,
 		DB:       0, // use default DB
 	})
 
 	// Ping Redis to make sure its working
-	ping, err := client.Ping(redis_ctx).Result()
+	ping, err := redisClient.Ping(redis_ctx).Result()
 	if err != nil {
 		log.Fatal(err.Error())
 		os.Exit(1) // exit
@@ -87,6 +116,23 @@ func main() {
 		log.Fatalf("Expected PONG instead got '%s'", ping)
 		os.Exit(1) // exit
 	}
+
+	// Creating MongoDB Client
+	// Creating mongodb connection string
+	URI := fmt.Sprintf("mongodb://%s:%s@%s:%d", mongoUsername, mongoPassword, "mongo-server", 27017)
+	mongoClient, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(URI))
+	if err != nil {
+		log.Fatal(err.Error())
+		os.Exit(1) // exit
+	}
+
+	err = mongoClient.Ping(context.TODO(), readpref.Primary())
+	if err != nil {
+		log.Fatal(err.Error())
+		os.Exit(1) // exit
+	}
+
+	usersCollection = mongoClient.Database(mongoDbName).Collection(mongoCollectionName)
 
 	// Setting up HTTP server routes
 	http.HandleFunc("/hello", hello)
