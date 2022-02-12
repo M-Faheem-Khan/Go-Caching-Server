758a34974747a44d443fd1d862c66181873b8aa2
Muhammad Faheem Khan <faheem5948@gmail.com>
2022-02-11 23:29:05 -0500

    updated test name

--- src/main_test.go
+++ src/main_test.go
@@ -12,16 +12,16 @@ func TestGetUserID(t *testing.T) {
 		want    int
 		wantErr bool
 	}{
-		{name: "No UserID Given - without trailing slash", args: args{path: "/users"}, want: 0, wantErr: true},
-		{name: "No UserID Given - with trailing slash", args: args{path: "/users/"}, want: 0, wantErr: true},
+		{name: "no_userId_given_without_trailing_slash", args: args{path: "/users"}, want: 0, wantErr: true},
+		{name: "no_userId_given_with_trailing_slash", args: args{path: "/users/"}, want: 0, wantErr: true},
 
-		{name: "UserID Given - without trailing slash", args: args{path: "/users/1234"}, want: 1234, wantErr: false},
-		{name: "UserID Given - with trailing slash", args: args{path: "/users/1234/"}, want: 1234, wantErr: false},
+		{name: "userId_given_without_trailing_slash", args: args{path: "/users/1234"}, want: 1234, wantErr: false},
+		{name: "userId_given_with_trailing_slash", args: args{path: "/users/1234/"}, want: 1234, wantErr: false},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got, err := GetUserID(tt.args.path)
+			got, err := getUserID(tt.args.path)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("GetUserID() error = %v, wantErr %v", err, tt.wantErr)
 				return
